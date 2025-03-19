dea254df5bfed973c9cf905c121662a1ab9bce77
Kenny <141840228+MarkKheanViari@users.noreply.github.com>
2025-03-19 16:10:51 +0800

    Change Password

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id ("kotlin-parcelize")
-    id ("kotlin-android")
-
+    id("kotlin-parcelize")
 }
 
 android {
@@ -17,7 +15,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -43,16 +40,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    // Corrected packaging block for Kotlin DSL
+    packaging {
+        resources {
+            excludes.add("/META-INF/DEPENDENCIES")
+        }
+    }
 }
 
 dependencies {
+    // Twilio for SMS
+    implementation("com.twilio.sdk:twilio:9.2.3")
 
-    implementation ("androidx.core:core:1.12.0")
-
-    implementation ("com.android.volley:volley:1.2.1")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
     // Core Libraries
-    implementation ("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -62,22 +63,17 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
 
-    // Networking and JSON Parsing
+    // Networking
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.android.volley:volley:1.2.1")
     implementation("org.json:json:20231013")
 
     // Image Loading
     implementation("com.github.bumptech.glide:glide:4.15.0")
-    implementation(libs.play.services.analytics.impl)
-    implementation(libs.androidx.tools.core)
-    implementation(libs.espresso.core) // Updated Glide
     kapt("com.github.bumptech.glide:compiler:4.15.0")
 
     // Testing Libraries
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation ("com.google.android.material:material:1.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.9.2")
 }
